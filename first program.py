96de890f787b4ea4693aab07459e8e92400ce18f
Dyeshi <lhendrupyeshi918@gmail.com>
2025-07-29 08:46:27 +0000

    my second commit

--- first program.py
+++ first program.py
@@ -1,2 +1,7 @@
-print('yeshi')
-print('hello world')
+a = 3
+b = 6
+sum = a + b
+print("The sum of a and b is:", sum)
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("your name",name,"and you are" +age+ "years old")
