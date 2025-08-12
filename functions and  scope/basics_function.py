2b467cf0f97f10472639dfe9d22b0f33a9d919e9
Dyeshi <lhendrupyeshi918@gmail.com>
2025-08-12 02:09:20 -0700

    basicfunction exe

---
+++ functions and  scope/basics_function.py
@@ -0,0 +1,20 @@
+def greet():
+    print("hello, world!")
+greet()
+def greet(name):
+    print(f"hello, {name}!")
+greet("yeshi")
+def square(number):
+    return number **5
+result = square(5)
+print(result)
+def is_even(number):
+    return number % 2 == 0
+print(is_even(4))
+print(is_even(7))
+def print_numbers(n):
+    for i in range(1, n + 1):
+        print(i)
+
+print_numbers(5)
+    
